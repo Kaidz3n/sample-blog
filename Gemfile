6987121d50475b488a2eb25a7f1fd933963a19f2
Fil <vtl.filimonov@gmail.com>
2017-07-09 18:58:51 +0300

    Add factory girl

--- Gemfile
+++ Gemfile
@@ -40,7 +40,8 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem "shoulda-matchers"
-  gem "rspec-rails", "~> 3.5"
+  gem "rspec-rails"
+  gem "factory_girl_rails"
 end
 
 group :development do
@@ -50,6 +51,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
@@ -44,6 +45,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
